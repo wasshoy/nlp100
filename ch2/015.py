7e970ff055bcdba9320cb76d593508323f3ef1ce
A.Arimura <404ari@gmail.com>
2020-05-29 00:53:19 +0900

    [update] 15の解答追加

---
+++ ch2/015.py
@@ -0,0 +1,9 @@
+import sys
+import pandas as pd
+
+if len(sys.argv) < 2:
+    print('Set arg number of line.')
+    sys.exit()
+n = int(sys.argv[1])
+df = pd.read_csv('./data/popular-names.txt', header=None, sep='\t')
+print(df.tail(n))
