2d2d6cd4213ea2de286420a3f971254f6f408d81
A.Arimura <404ari@gmail.com>
2020-06-05 16:28:20 +0900

    [update] 解答追加

--- ch3/020.py
+++ ch3/020.py
@@ -2,9 +2,10 @@ import json
 import pandas as pd
 
 
+# .jsonlファイルや一行のjsonはkines=Trueで読み込む必要がある
 df = pd.read_json('./input/raw/jawiki-country.json', lines=True)
 # print(df.head())
-print(df.query('title == "イギリス"')['text'].values[0])
-
-# 21~29で使用するため保存
-df.to_json('./input/processing/jawiki-irish-text.json')
+print(df.query('title == "イギリス"')['text'].values)
+# print(type(df.query('title == "イギリス"')['text'].values))
+# for data in df.query('title == "イギリス"')['text'].values:
+#     print(data)
---
+++ ch3/021.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import re
+
+
+def main():
+    df = pd.read_json('./input/raw/jawiki-country.json', lines=True)
+    uk_data = df.query('title == "イギリス"')['text'].values
+    # print(type(uk_df))
+    uk_text_lines = uk_text[0].split('\n')  # ndarrayなので中身のstrを取り出し改行で区切る
+    # 正規表現モジュールreを用いて"Category"を含む文字列を抽出
+    pattern = re.compile('Category')
+    categories = [line for line in uk_text_lines if re.search(pattern, line)]
+    print(*categories, sep='\n')
+
+
+# filterを使った別解
+def ano_solution():
+    df = pd.read_json('./input/raw/jawiki-country.json', lines=True)
+    uk_text = df.query('title == "イギリス"')['text'].values
+    uk_text_lines = uk_text[0].split('\n')  # ndarrayなので中身のstrを取り出し改行で区切る
+    categories = list(filter(lambda s: 'Category' in s, uk_text_lines))
+    print(*categories, sep='\n')
+
+
+ano_solution()
